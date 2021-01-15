809072d514b1867b71cbc08c45154206c777b227
Shogo <vanitas710@yahoo.co.jp>
2021-01-16 05:44:26 +0900

    Initial Commit

---
+++ AlertController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertController
+//
+//  Created by Shogo Nobuhara on 2021/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
